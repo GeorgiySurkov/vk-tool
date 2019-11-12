562d0113b0b20ea9e0edaea0665ecedaf7b036aa
GeorgiySurkov <georgiy.surkov@gmail.com>
2019-11-12 17:38:43 +0500

    new_from_user_friends method

--- vk_tool/users_set.py
+++ vk_tool/users_set.py
@@ -1,5 +1,5 @@
 from typing import Set
-from vk_tool import User
+from vk_tool import User, vk
 
 
 class UsersSet:
@@ -7,8 +7,14 @@ class UsersSet:
         self.s = s
 
     @staticmethod
-    def new_from_user_friends(user):
-        pass
+    def new_from_user_friends(user: User):
+        response = vk.method('friends.get', values={
+            'user_id': user.id,
+        })
+        friends = set()
+        for user in response:
+            friends.add(User(user))
+        return UsersSet(friends)
 
     @staticmethod
     def new_from_public_subscribers(public):
