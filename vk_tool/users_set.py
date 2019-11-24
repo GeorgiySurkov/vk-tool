d8e3381355c5e67fb70bc8e97439fff6c9f83503
GeorgiySurkov <georgiy.surkov@gmail.com>
2019-11-24 23:58:34 +0500

    rewrite import, new_from_user_friends method

--- vk_tool/users_set.py
+++ vk_tool/users_set.py
@@ -1,5 +1,6 @@
 from typing import Set
-from vk_tool import User, vk
+from vk_tool import vk, USER_FIELDS
+from vk_tool.user import User
 
 
 class UsersSet:
@@ -11,11 +12,21 @@ class UsersSet:
         response = vk.method('friends.get', values={
             'user_id': user.id,
         })
-        friends = set()
-        for user in response:
-            friends.add(User(user))
+        friends = set(
+            map(
+                lambda user_dict: User(user_dict), 
+                vk.method('users.get', values={
+                    'user_ids': ','.join(map(str, response['items'])),
+                    'fields': USER_FIELDS,
+                    'name_case': 'Nom',
+                }),
+            )
+        )
         return UsersSet(friends)
 
     @staticmethod
-    def new_from_public_subscribers(public):
+    def new_from_community_subscribers(public):
         pass
+
+    def __repr__(self):
+        return self.s.__repr__()
